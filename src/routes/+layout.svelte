1e66b6c12f55c445c73f4131d60d7ec8acc49855
Aravinda <apranthilaka@outlook.com>
2026-01-22 20:51:59 +0530

    app frames

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,33 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import '../app.css';
+    import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="main-wrapper border h-screen w-screen">
+    <div class="top-menu border flex justify-start items-center px-6">
+        Top menu
+    </div>
+    <div class="left-menu border">Left menu</div>
+    <div class="border">
+        {@render children()}
+    </div>
+</div>
+
+<style>
+    .main-wrapper {
+        display: grid;
+        grid-template-columns: 250px 1fr;
+        grid-template-rows: 56px 1fr;
+    }
+    .top-menu {
+        grid-column: 2/3;
+        grid-row: 1/2;
+    }
+    .left-menu {
+        grid-column: 1/2;
+        grid-row: 1/3;
+    }
+</style>
