f6c63fe763531b32ab7488ca1105729219442134
Aravinda <apranthilaka@outlook.com>
2026-02-03 22:59:39 +0530

    removed themes

--- src/lib/components/TopMenu.svelte
+++ src/lib/components/TopMenu.svelte
@@ -1,14 +1,10 @@
 <script>
-    import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
 <div
     class="top-menu border-b border-gray-200 flex justify-start items-center px-6 h-14 text-sm"
 >
     top menu
-    <!-- <nav class="p-4 flex justify-end">
-        <ThemeToggle />
-    </nav> -->
 </div>
 
 <style>
