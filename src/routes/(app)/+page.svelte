cd3306a0dd3ad71afff566ca363bfafb7d2edc77
Aravinda <apranthilaka@outlook.com>
2026-02-05 12:53:59 +0530

    update

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -9,32 +9,43 @@
             designs are still behind the scenes, but the full collection will be
             live soon.
         </div>
-        <div class="mb-4 flex flex-col items-center">
+        <!-- <div class="mb-4 flex flex-col items-center">
             <div class="font-medium text-lg text-gray-800">Hi! Im Aravinda</div>
             <div class=" text-gray-950 max-w-xl text-center">
                 I design & develop things that look great and scale even better.
             </div>
-        </div>
-        <div class="text-center text-sm text-gray-600 mb-4">
-            I focus on creating "Tailwind-ready" design systems in Figma that
-            are built for flexibility. From there, I translate those designs
-            into high-performance, functional components using SvelteKit. The
-            proof is in the build: this entire portfolio is a live demonstration
-            of my stack, developed with SvelteKit, hosted on Cloudflare, and
-            powered by Supabase. <br /> <br /> Explore my design systems and developed
-            components below.
+        </div> -->
+        <div class="text-center text-gray-800 mb-4">
+            <span class="font-medium">
+                Design & develop things that look great and scale even better.
+            </span>
+            <br />
+
+            I build Tailwind-ready design systems in Figma, specifically
+            structured for a seamless transition to code. By translating these
+            systems into SvelteKit, I deliver high-performance, browser-native
+            prototypes that capture the true look, feel, and interactivity of
+            the final product.
+            <br /><br />
+            <span class="text-sm text-gray-950 line-clamp-">
+                The proof is in the build: this entire portfolio is a live
+                demonstration of my stack, developed with <span
+                    class="font-medium">SvelteKit</span
+                >, hosted on <span class="font-medium">Cloudflare</span>, and
+                powered by <span class="font-medium">Supabase</span>.
+            </span>
         </div>
         <div class="mt-4 flex flex-row gap-2 w-full">
             <a
-                class="border border-gray-300 text-xs px-5 h-8 flex items-center justify-center rounded-full text-gray-900 hover:text-gray-900 hover:bg-gray-100 hover:font-medium transition-all delay-75 grow w-full"
+                class="border border-gray-300 px-5 h-8 flex items-center justify-center rounded-full text-gray-800 hover:text-gray-900 hover:bg-gray-100 transition-all delay-75 grow w-full"
                 href="../cards/cardsa">Components</a
             >
             <a
-                class="border border-gray-300 text-xs px-5 h-8 flex items-center justify-center rounded-full text-gray-900 hover:text-gray-900 hover:bg-gray-100 hover:font-medium transition-all delay-75 grow w-full"
+                class="border border-gray-300 px-5 h-8 flex items-center justify-center rounded-full text-gray-800 hover:text-gray-900 hover:bg-gray-100 transition-all delay-75 grow w-full"
                 href="../forms/formsa">Forms</a
             >
             <a
-                class="border border-gray-300 text-xs px-5 h-8 flex items-center justify-center rounded-full text-gray-900 hover:text-gray-900 hover:bg-gray-100 hover:font-medium transition-all delay-75 grow w-full"
+                class="border border-gray-300 px-5 h-8 flex items-center justify-center rounded-full text-gray-800 hover:text-gray-900 hover:bg-gray-100 transition-all delay-75 grow w-full"
                 href="../figma/figmacards">Figma Designs</a
             >
         </div>
