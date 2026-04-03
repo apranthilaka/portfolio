db6b16a21bea7dab141e927ab0754aaaec5919aa
Aravinda <apranthilaka@outlook.com>
2026-04-03 10:32:04 +0530

    update

--- src/lib/cover-components/ProjectTwo.svelte
+++ src/lib/cover-components/ProjectTwo.svelte
@@ -19,7 +19,7 @@
             <iframe
                 title=""
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63371.81536310977!2d79.81500565289788!3d6.921836877837569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae253d10f7a7003%3A0x320b2e4d32d3838d!2sColombo!5e0!3m2!1sen!2slk!4v1775186766375!5m2!1sen!2slk"
-                class="w-full h-full border-none border-0 rounded-lg shadow-none"
+                class="w-full h-full border-none border-0 rounded-lg text-shadow-none"
                 allowfullscreen
                 loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"
