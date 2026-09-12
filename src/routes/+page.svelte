310dbb377d0563d30e4ce0be66aca4fd9a278b79
Aravinda <apranthilaka@outlook.com>
2026-09-12 10:25:14 +0530

    update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<div
+    class="w-full h-screen flex flex-col justify-center items-center text-center text-sm bg-slate-50"
+>
+    <div class="w-10">
+        <img src="logo.png" alt="" />
+    </div>
+    <div class="font-medium text-lg mb-2">Launching Soon.</div>
+    <div class="max-w-110 mb-4">
+        Interactive web implementations of my <a
+            target="_blank"
+            class="underline"
+            href="https://apranthilaka.com/">Figma designs</a
+        >, developed through hybrid manual coding and AI collaboration.
+    </div>
+    <div class="text-sm text-neutral-400">
+        Status: Migrating component architecture
+    </div>
+</div>
